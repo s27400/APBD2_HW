f798e969196ad76b6125b18961aa5268a42c4c13
s27400 <s27400@pjwstk.edu.pl>
2024-03-13 21:33:37 +0100

    Max added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,12 +13,27 @@
         res = res / counter;
         return res;
     }
+
+    public static int Max(int[] tab)
+    {
+        int max = 0;
+        foreach (int a in tab)
+        {
+            if (max < a)
+            {
+                max = a;
+            }
+        }
+
+        return max;
+    }
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
 
-        int[] tbl = { 1, 2, 3, 4 };
+        int[] tbl = { 1, 2, 3, 4, 0, 10, 5 };
 
         Console.WriteLine(Avg(tbl));
+        Console.WriteLine(Max(tbl));
     }
 }
